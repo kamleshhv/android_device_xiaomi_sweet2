4557a4d0cfcba7564b2dd639cecfaa2d55e8e02c
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-08-13 10:31:37 +0530

    sweet2: switch to sleepy kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone -b lineage-21 https://github.com/Lafactorial/android_vendor_xiaomi_swe
 git clone -b lineage-21 https://github.com/Lafactorial/android_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
 
 # kernel 
-git clone --depth=1 -b sleepy-k6a-inline https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone --depth=1 -b sleepy-k6a-inline https://github.com/kamleshhv/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 
 # hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
